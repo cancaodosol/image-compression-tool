b4d4f368efc99b3101b4c5e4120ee965a46acf5f
Hideyuki Matsui <atagohan@yahoo.co.jp>
2025-10-26 11:46:49 +0900

    add: バージョン表記をするように。（Git Pagesに反映されたか分かりづらいから。）

---
+++ tool-version.js
@@ -0,0 +1,9 @@
+const version = 'v1.0.1';
+
+const h1 = document.querySelector('h1');
+const div = document.createElement('span');
+div.style.fontSize = '0.4em';
+div.style.marginLeft = '4px';
+div.textContent = `${version}`;
+
+h1.appendChild(div);
